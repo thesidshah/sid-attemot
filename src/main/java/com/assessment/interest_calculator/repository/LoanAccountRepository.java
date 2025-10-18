5285e183ed30cea16a0493bd6067ff8c44a99e8d
thesidshah <siddhant997@gmail.com>
2025-10-18 14:15:11 -0400

    imported the wrong pageable

--- src/main/java/com/assessment/interest_calculator/repository/LoanAccountRepository.java
+++ src/main/java/com/assessment/interest_calculator/repository/LoanAccountRepository.java
@@ -1,6 +1,12 @@
 package com.assessment.interest_calculator.repository;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.assessment.interest_calculator.entity.LoanAccount;
@@ -11,5 +17,16 @@ import com.assessment.interest_calculator.entity.LoanAccount;
  */
 @Repository
 public interface LoanAccountRepository extends JpaRepository<LoanAccount, Long> {
-    
+    /**
+     * Find all accounts that need interest application for a given date.
+     * This method should support pagination for batch processing.
+     * 
+     * @param forDate The date for which to find accounts needing interest application.
+     * @param pageable The pagination information.
+     * @return A page of loan accounts needing interest application.
+     */
+    @Query("SELECT la FROM LoanAccount la WHERE " +
+           "(la.lastInterestAppliedDate IS NULL"+ 
+           " OR la.lastInterestAppliedDate < :forDate)")
+           Page<LoanAccount> findAccountsNeedingInterestApplication(@Param("forDate")LocalDate forDate, Pageable pageable);
 }
