33e47be48a9f688e702d598c665e799fce8193fd
thesidshah <siddhant997@gmail.com>
2025-10-18 13:06:21 -0400

    creating the config file for repeating jobs

---
+++ src/main/java/com/assessment/interest_calculator/config/InterestScheduler.java
@@ -0,0 +1,13 @@
+package com.assessment.interest_calculator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@EnableScheduling
+@Slf4j
+public class InterestScheduler {
+    
+}
