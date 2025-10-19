6eb104fb6b5211824dc6e791ce384d6ef97030c6
thesidshah <siddhant997@gmail.com>
2025-10-18 23:54:45 -0400

    need account types

--- src/main/java/com/assessment/interest_calculator/controller/AccountController.java
+++ src/main/java/com/assessment/interest_calculator/controller/AccountController.java
@@ -1,7 +1,10 @@
 package com.assessment.interest_calculator.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.assessment.interest_calculator.repository.LoanAccountRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,5 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/accounts")
 @Slf4j
 public class AccountController {
-    
+      private final LoanAccountRepository loanAccountRepository;
+      public AccountController(LoanAccountRepository loanAccountRepository) {
+            this.loanAccountRepository = loanAccountRepository;
+      }
+
 }
