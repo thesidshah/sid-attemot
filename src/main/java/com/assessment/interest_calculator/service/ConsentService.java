1a8b904def6b6c8914f577bb4e05e3f7fd2660b4
thesidshah <siddhant997@gmail.com>
2025-11-06 10:38:36 -0500

    Getting the date tine right took effort? god.

--- src/main/java/com/assessment/interest_calculator/service/ConsentService.java
+++ src/main/java/com/assessment/interest_calculator/service/ConsentService.java
@@ -1,6 +1,11 @@
 package com.assessment.interest_calculator.service;
 
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Service;
+
+import com.assessment.interest_calculator.config.DigioAAProperties;
+import com.assessment.interest_calculator.repository.ConsentRequestRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,5 +14,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class ConsentService {
-    
+    private final DigioAAClient digioAAClient;
+    private final ConsentRequestRepository consentRequestRepository;
+    private final DigioAAProperties digioAAProperties;
+
+    private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
 }
