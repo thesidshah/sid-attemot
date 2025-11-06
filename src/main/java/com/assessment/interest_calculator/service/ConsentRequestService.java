901402a2231b4f6a94c3049fe116b422d0d04deb
thesidshah <siddhant997@gmail.com>
2025-11-06 10:20:14 -0500

    still have to write the actual consent service

--- src/main/java/com/assessment/interest_calculator/service/ConsentRequestService.java
+++ src/main/java/com/assessment/interest_calculator/service/ConsentRequestService.java
@@ -1,5 +1,13 @@
 package com.assessment.interest_calculator.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class ConsentRequestService {
     
 }
